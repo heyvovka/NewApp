b2f815bcde5832ed338c8c6aa73023c5bbe2ad7b
Vladimir S <heyvovka@mail.ru>
2023-04-27 17:26:57 +0700

    Initial Commit

---
+++ NewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewApp
+//
+//  Created by Vladimir Savorovsky on 27.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
