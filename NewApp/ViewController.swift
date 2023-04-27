da0e6021514311ef8876dc63dc7e252b824b7010
Vladimir S <heyvovka@mail.ru>
2023-04-27 20:59:31 +0700

    New features added

--- NewApp/ViewController.swift
+++ NewApp/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var isSun: Bool = false
+    
+    @IBOutlet weak var changeButton: UIButton!
+    @IBOutlet weak var skyImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        skyImageView.image = UIImage(systemName: "moon")
     }
-
-
+    @IBAction func buttonDidTap(_ sender: Any) {
+        if isSun { // 1
+            skyImageView.image = UIImage(systemName: "moon") // 2
+        } else {
+            skyImageView.image = UIImage(systemName: "sun.max") // 3
+        }
+        isSun.toggle() // 4
+    }
 }
 
